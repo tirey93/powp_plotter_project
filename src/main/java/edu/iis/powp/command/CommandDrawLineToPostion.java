4272e19cdc813132ec336c495eec515797ed4e50
tirey93 <tirey93@gmail.com>
2018-11-18 16:33:38 +0100

    3.1.1 wzorzec command

---
+++ src/main/java/edu/iis/powp/command/CommandDrawLineToPostion.java
@@ -0,0 +1,13 @@
+package edu.iis.powp.command;
+
+import edu.iis.powp.app.DriverManager;
+
+public class CommandDrawLineToPostion implements PlotterCommand {
+
+	@Override
+	public void execute(DriverManager driver) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/edu/iis/powp/command/PlotterCommand.java
@@ -0,0 +1,7 @@
+package edu.iis.powp.command;
+
+import edu.iis.powp.app.DriverManager;
+
+public interface PlotterCommand {
+	public void execute(DriverManager driver);
+}
