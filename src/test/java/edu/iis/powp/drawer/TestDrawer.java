8fe2da42e41a969b0686659d0d4780a0fad941b2
tirey93 <tirey93@gmail.com>
2018-11-18 17:32:37 +0100

    3.1.3

--- src/test/java/edu/iis/powp/drawer/TestDrawer.java
+++ src/test/java/edu/iis/powp/drawer/TestDrawer.java
@@ -1,5 +1,11 @@
 package edu.iis.powp.drawer;
 
+import edu.iis.client.plottermagic.IPlotter;
+import edu.iis.powp.adapter.PlotterSimulator;
+import edu.iis.powp.command.CommandDrawLineToPostion;
+import edu.iis.powp.command.CommandSetPosition;
+import edu.iis.powp.command.ComplexCommand;
+import edu.iis.powp.features.DrawerFeature;
 import edu.kis.powp.drawer.panel.DefaultDrawerFrame;
 import edu.kis.powp.drawer.panel.DrawPanelController;
 import edu.kis.powp.drawer.shape.ILine;
@@ -20,6 +26,17 @@ public class TestDrawer {
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(-100, -60);
 		line.setEndCoordinates(60, 130);
-		controller.drawLine(line);
+		IPlotter plotter = new PlotterSimulator(controller);
+		
+		CommandSetPosition c1 = new CommandSetPosition(0, 0);
+		
+		ComplexCommand square = new ComplexCommand();
+		square.addCommand(c1);
+		square.addCommand(new CommandDrawLineToPostion(50, 0));
+		square.addCommand(new CommandDrawLineToPostion(50, 50));
+		square.addCommand(new CommandDrawLineToPostion(0, 50));
+		square.addCommand(new CommandDrawLineToPostion(0, 0));
+		square.execute(plotter);
+		//controller.drawLine(line);
 	}
 }
